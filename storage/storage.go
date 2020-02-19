d6118ef00f2a2a5d64368637fead1afccd5d2c1e
Nick Kozlov <nk@buttonwallet.com>
2020-02-19 23:31:28 +0300

    Updates from atlas/master

--- storage/storage.go
+++ storage/storage.go
@@ -7,24 +7,11 @@ import (
 
 type Storage struct {
 	redis.Redis
-	blockHeights BlockMap
 }
 
 func New() *Storage {
 	s := new(Storage)
-	s.blockHeights.heights = make(map[uint]int64)
 	return s
-}
-
-type Tracker interface {
-	GetBlockNumber(coin uint) (int64, error)
-	SetBlockNumber(coin uint, num int64) error
-}
-
-type Addresses interface {
-	Lookup(coin uint, addresses []string) ([]blockatlas.Subscription, error)
-	AddSubscriptions(subscriptions []blockatlas.Subscription)
-	DeleteSubscriptions(subscriptions []blockatlas.Subscription)
 }
 
 type Market interface {
