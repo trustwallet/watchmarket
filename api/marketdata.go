baa6231a49d4e0a72c84e14ca8b7b06ff6beb228
Nick Kozlov <Enormousrage@gmail.com>
2020-03-08 19:48:28 +0300

    Temporary return gin cache middleware

--- api/marketdata.go
+++ api/marketdata.go
@@ -7,6 +7,7 @@ import (
 	"github.com/trustwallet/blockatlas/coin"
 	"github.com/trustwallet/blockatlas/pkg/blockatlas"
 	"github.com/trustwallet/blockatlas/pkg/ginutils"
+	"github.com/trustwallet/blockatlas/pkg/ginutils/gincache"
 	"github.com/trustwallet/blockatlas/pkg/logger"
 	"github.com/trustwallet/watchmarket/market"
 	"github.com/trustwallet/watchmarket/pkg/watchmarket"
@@ -16,6 +17,7 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
 )
 
 const (
@@ -38,7 +40,7 @@ func SetupMarketAPI(router gin.IRouter, db storage.Market) {
 	// Ticker
 	router.POST("/ticker", getTickersHandler(db))
 	// Charts
-	router.GET("/charts", getChartsHandler())
+	router.GET("/charts", gincache.CacheMiddleware(time.Minute*5, getChartsHandler()))
 	router.GET("/info", getCoinInfoHandler())
 }
 
