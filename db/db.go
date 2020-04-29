bac938abe1d8e724eed84b58182d58f3feb314a7
Nick Kozlov <enormousrage@gmail.com>
2020-04-29 21:58:26 +0300

    Create a subscription draft

---
+++ db/db.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/trustwallet/watchmarket/db/models"
+)
+
+type Instance struct {
+	Gorm *gorm.DB
+}
+
+func New(uri string) (*Instance, error) {
+	g, err := gorm.Open("postgres", uri)
+	if err != nil {
+		return nil, err
+	}
+
+	g.AutoMigrate(
+		&models.Subscription{},
+	)
+
+	i := &Instance{Gorm: g}
+
+	return i, nil
+}
