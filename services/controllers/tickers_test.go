225f0cec8fa4372434ab52398f04fadece29e49c
Nick Kozlov <enormousrage@gmail.com>
2020-05-16 20:15:58 +0300

    Made controller test for tickers

--- services/controllers/tickers_test.go
+++ services/controllers/tickers_test.go
@@ -1,1 +1,18 @@
 package controllers
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestController_HandleTickersRequest(t *testing.T) {
+	c := setupController(t)
+	assert.NotNil(t, c)
+
+	tickers, err := c.HandleTickersRequest(TickerRequest{
+		Currency: "USD",
+		Assets:   []Coin{{Coin: 60, TokenId: "A"}, {Coin: 714, TokenId: "A"}},
+	})
+	assert.Nil(t, err)
+	assert.NotNil(t, tickers)
+}
