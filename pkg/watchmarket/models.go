d2cbe266939baa8bdc16f69e32ea8fa88a103461
Nick Kozlov <22479658+EnoRage@users.noreply.github.com>
2020-06-24 05:31:49 +0300

    Change provider_link to omitempty

--- pkg/watchmarket/models.go
+++ pkg/watchmarket/models.go
@@ -60,7 +60,7 @@ type (
 
 	CoinDetails struct {
 		Provider          string  `json:"provider"`
-		ProviderURL       string  `json:"provider_url"`
+		ProviderURL       string  `json:"provider_url,omitempty"`
 		Vol24             float64 `json:"volume_24"`
 		MarketCap         float64 `json:"market_cap"`
 		CirculatingSupply float64 `json:"circulating_supply"`
