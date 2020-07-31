423da8250343383ae89039a8f4de727651e9438e
Nick Kozlov <22479658+EnoRage@users.noreply.github.com>
2020-07-31 19:57:03 +0300

    Finish with tests for worker memory

--- services/worker/memory_test.go
+++ services/worker/memory_test.go
@@ -15,8 +15,15 @@ import (
 func TestWorker_SaveRatesToMemory(t *testing.T) {
 	c := config.Init("../../config/test.yml")
 	assert.NotNil(t, c)
-	dbMock := getDbMock()
+
+	testRatesBasic(t, c)
+	testRatesShowOptionAlways(t, c)
+	testRatesShowOptionNever(t, c)
+}
+
+func testRatesBasic(t *testing.T, c config.Configuration) {
 	now := time.Now()
+	dbMock := getDbMock()
 	dbMock.WantedRates = []models.Rate{
 		{
 			Currency:         "USD",
@@ -58,7 +65,11 @@ func TestWorker_SaveRatesToMemory(t *testing.T) {
 		Rate:             1.5,
 		Timestamp:        now.Unix(),
 	}, res)
+}
 
+func testRatesShowOptionAlways(t *testing.T, c config.Configuration) {
+	now := time.Now()
+	dbMock := getDbMock()
 	dbMock.WantedRates = []models.Rate{
 		{
 			Currency:         "USD",
@@ -100,7 +111,11 @@ func TestWorker_SaveRatesToMemory(t *testing.T) {
 		Rate:             1,
 		Timestamp:        now.Unix(),
 	}, res2)
+}
 
+func testRatesShowOptionNever(t *testing.T, c config.Configuration) {
+	now := time.Now()
+	dbMock := getDbMock()
 	dbMock.WantedRates = []models.Rate{
 		{
 			Currency:         "USD",
@@ -142,11 +157,21 @@ func TestWorker_SaveRatesToMemory(t *testing.T) {
 		Rate:             2,
 		Timestamp:        now.Unix(),
 	}, res3)
+
 }
 
 func TestWorker_SaveTickersToMemory(t *testing.T) {
 	c := config.Init("../../config/test.yml")
 	assert.NotNil(t, c)
+
+	testTickersBasic(t, c)
+	testTickersShowOptionNever(t, c)
+	testTickersShowOptionAlways(t, c)
+	testTickersOutdated(t, c)
+	testTickersVolume(t, c)
+}
+
+func testTickersBasic(t *testing.T, c config.Configuration) {
 	dbMock := getDbMock()
 	now := time.Now()
 	dbMock.WantedTickers = []models.Ticker{
@@ -200,6 +225,248 @@ func TestWorker_SaveTickersToMemory(t *testing.T) {
 			Currency:  "USD",
 			Provider:  "coinmarketcap",
 			Value:     11,
+		},
+		LastUpdate: res.LastUpdate,
+	}, res)
+	assert.Equal(t, res.LastUpdate.Unix(), now.Unix())
+}
+
+func testTickersShowOptionNever(t *testing.T, c config.Configuration) {
+	dbMock := getDbMock()
+	now := time.Now()
+	dbMock.WantedTickers = []models.Ticker{
+		{
+			ID:          "c1",
+			Currency:    "USD",
+			Provider:    "coinmarketcap",
+			ShowOption:  2,
+			Coin:        1,
+			TokenId:     "",
+			Change24h:   1,
+			Value:       11,
+			LastUpdated: now,
+		},
+		{
+			ID:          "c1",
+			Currency:    "USD",
+			Provider:    "coingecko",
+			ShowOption:  0,
+			Coin:        1,
+			TokenId:     "",
+			Change24h:   2,
+			Value:       12,
+			LastUpdated: now,
+		},
+		{
+			ID:          "c1",
+			Currency:    "USD",
+			Provider:    "binancedex",
+			Coin:        1,
+			TokenId:     "",
+			Change24h:   1,
+			Value:       14,
+			ShowOption:  0,
+			LastUpdated: now,
+		},
+	}
+
+	w := Init(nil, nil, dbMock, memory.Init(), c)
+	w.SaveTickersToMemory()
+	resRaw, err := w.cache.Get("c1", context.Background())
+	assert.Nil(t, err)
+
+	var res watchmarket.Ticker
+	assert.Nil(t, json.Unmarshal(resRaw, &res))
+	assert.Equal(t, watchmarket.Ticker{
+		Coin:    1,
+		TokenId: "",
+		Price: watchmarket.Price{
+			Change24h: 2,
+			Currency:  "USD",
+			Provider:  "coingecko",
+			Value:     12,
+		},
+		LastUpdate: res.LastUpdate,
+	}, res)
+	assert.Equal(t, res.LastUpdate.Unix(), now.Unix())
+}
+
+func testTickersShowOptionAlways(t *testing.T, c config.Configuration) {
+	dbMock := getDbMock()
+	now := time.Now()
+	dbMock.WantedTickers = []models.Ticker{
+		{
+			ID:          "c1",
+			Currency:    "USD",
+			Provider:    "coinmarketcap",
+			ShowOption:  2,
+			Coin:        1,
+			TokenId:     "",
+			Change24h:   1,
+			Value:       11,
+			LastUpdated: now,
+		},
+		{
+			ID:          "c1",
+			Currency:    "USD",
+			Provider:    "coingecko",
+			ShowOption:  0,
+			Coin:        1,
+			TokenId:     "",
+			Change24h:   2,
+			Value:       12,
+			LastUpdated: now,
+		},
+		{
+			ID:          "c1",
+			Currency:    "USD",
+			Provider:    "binancedex",
+			Coin:        1,
+			TokenId:     "",
+			Change24h:   1,
+			Value:       14,
+			ShowOption:  1,
+			LastUpdated: now,
+		},
+	}
+
+	w := Init(nil, nil, dbMock, memory.Init(), c)
+	w.SaveTickersToMemory()
+	resRaw, err := w.cache.Get("c1", context.Background())
+	assert.Nil(t, err)
+
+	var res watchmarket.Ticker
+	assert.Nil(t, json.Unmarshal(resRaw, &res))
+	assert.Equal(t, watchmarket.Ticker{
+		Coin:    1,
+		TokenId: "",
+		Price: watchmarket.Price{
+			Change24h: 1,
+			Currency:  "USD",
+			Provider:  "binancedex",
+			Value:     14,
+		},
+		LastUpdate: res.LastUpdate,
+	}, res)
+	assert.Equal(t, res.LastUpdate.Unix(), now.Unix())
+}
+
+func testTickersOutdated(t *testing.T, c config.Configuration) {
+	dbMock := getDbMock()
+	now := time.Now()
+	dbMock.WantedTickers = []models.Ticker{
+		{
+			ID:          "c1",
+			Currency:    "USD",
+			Provider:    "coinmarketcap",
+			ShowOption:  2,
+			Coin:        1,
+			TokenId:     "",
+			Change24h:   1,
+			Value:       11,
+			LastUpdated: time.Date(1999, 1, 1, 1, 1, 1, 1, time.Local),
+		},
+		{
+			ID:          "c1",
+			Currency:    "USD",
+			Provider:    "coingecko",
+			ShowOption:  0,
+			Coin:        1,
+			TokenId:     "",
+			Change24h:   2,
+			Value:       12,
+			LastUpdated: now,
+		},
+		{
+			ID:          "c1",
+			Currency:    "USD",
+			Provider:    "binancedex",
+			Coin:        1,
+			TokenId:     "",
+			Change24h:   1,
+			Value:       14,
+			ShowOption:  0,
+			LastUpdated: now,
+		},
+	}
+
+	w := Init(nil, nil, dbMock, memory.Init(), c)
+	w.SaveTickersToMemory()
+	resRaw, err := w.cache.Get("c1", context.Background())
+	assert.Nil(t, err)
+
+	var res watchmarket.Ticker
+	assert.Nil(t, json.Unmarshal(resRaw, &res))
+	assert.Equal(t, watchmarket.Ticker{
+		Coin:    1,
+		TokenId: "",
+		Price: watchmarket.Price{
+			Change24h: 2,
+			Currency:  "USD",
+			Provider:  "coingecko",
+			Value:     12,
+		},
+		LastUpdate: res.LastUpdate,
+	}, res)
+	assert.Equal(t, res.LastUpdate.Unix(), now.Unix())
+}
+
+func testTickersVolume(t *testing.T, c config.Configuration) {
+	dbMock := getDbMock()
+	now := time.Now()
+	c.RestAPI.Tickers.RespsectableVolume = 999
+	dbMock.WantedTickers = []models.Ticker{
+		{
+			ID:          "c1",
+			Currency:    "USD",
+			Provider:    "coinmarketcap",
+			ShowOption:  2,
+			Coin:        1,
+			TokenId:     "",
+			Change24h:   1,
+			Value:       11,
+			LastUpdated: now,
+		},
+		{
+			ID:          "c1",
+			Currency:    "USD",
+			Provider:    "coingecko",
+			ShowOption:  0,
+			Coin:        1,
+			TokenId:     "",
+			Change24h:   2,
+			Value:       12,
+			LastUpdated: now,
+		},
+		{
+			ID:          "c1",
+			Currency:    "USD",
+			Provider:    "binancedex",
+			Coin:        1,
+			TokenId:     "",
+			Change24h:   1,
+			Value:       14,
+			Volume:      1000,
+			ShowOption:  0,
+			LastUpdated: now,
+		},
+	}
+
+	w := Init(nil, nil, dbMock, memory.Init(), c)
+	w.SaveTickersToMemory()
+	resRaw, err := w.cache.Get("c1", context.Background())
+	assert.Nil(t, err)
+
+	var res watchmarket.Ticker
+	assert.Nil(t, json.Unmarshal(resRaw, &res))
+	assert.Equal(t, watchmarket.Ticker{
+		Coin:    1,
+		TokenId: "",
+		Price: watchmarket.Price{
+			Change24h: 1,
+			Currency:  "USD",
+			Provider:  "binancedex",
+			Value:     14,
 		},
 		LastUpdate: res.LastUpdate,
 	}, res)
