66b2b4356e2fe46ffa8715ab2d769fa79f1ca859
Viktor Radchenko <1641795+vikmeup@users.noreply.github.com>
2020-11-10 23:13:07 -0800

    Update init.go

--- internal/init.go
+++ internal/init.go
@@ -78,9 +78,8 @@ func InitEngine(ginMode string) *gin.Engine {
 
 func Logger() gin.HandlerFunc {
 	return gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
-		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+		return fmt.Sprintf("%s - \"%s %s %s %d %s \"%s\" %s\"\n",
 			param.ClientIP,
-			//param.TimeStamp.Format(time.RFC1123),
 			param.Method,
 			param.Path,
 			param.Request.Proto,
